5a1a7e3d3413c3285652ab5fdcd6420244da7608
suesho <35975764+suesho@users.noreply.github.com>
2022-03-30 10:28:15 +0900

    django.testを使ったテストクラスを追加

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class HogeTest(TestCase):
+    def test_hoge(self):
+        post = Post()
+        self.assertIsNotNone(post.created_date)
+        print('testです')
